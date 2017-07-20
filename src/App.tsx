67c03e667ca5dbf2300dd59f192fe3b3c2a9719d
Blair Knickerbocker <b.knickerbocker@shieldsafety.co.uk>
2017-07-20 11:59:34 +0100

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import * as React from "react";
+// import logo from './logo.svg';
+import "./App.css";
+
+
+const logo = require("./logo.svg") as string; // tslint:disable-line
+
+export class App extends React.Component<{}, {}>
+{
+  public render()
+  {
+    const text = "To get started, edit <code>src/App.js</code> and save to reload.";
+
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+        <p className="App-intro" dangerouslySetInnerHTML={{ __html: text }}>
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/Test.tsx
@@ -0,0 +1,16 @@
+import * as React from "react";
+
+export class Test extends React.Component<{}, {}>
+{
+    Test()
+    {
+
+    }
+
+    ThisIsAFunc(input: string): string
+    {
+        return input;
+    }
+}
+
+export default Test;
