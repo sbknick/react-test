45efa670a9b07eccc2128e048af03997eafaedc4
Blair Knickerbocker <b.knickerbocker@shieldsafety.co.uk>
2017-07-21 13:42:20 +0100

    Uncommented tests because they work now?

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,8 @@
-// import * as React from "react";
-// import * as ReactDOM from "react-dom";
-// import App from './App';
-// // import { it } from "mocha";
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import App from "./App";
 
-// it('renders without crashing', () => {
-//   const div = document.createElement('div');
-//   ReactDOM.render(<App />, div);
-// });
+it("renders without crashing", () => {
+  const div = document.createElement("div");
+  ReactDOM.render(<App />, div);
+});
